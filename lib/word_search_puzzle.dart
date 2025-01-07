7dd34b136ca0d7b9533d760e7f36cdddce9c9d8e
arw6329 <70402863+arw6329@users.noreply.github.com>
2025-01-06 17:58:56 -0800

    Implement highlight via user input

--- lib/word_search_puzzle.dart
+++ lib/word_search_puzzle.dart
@@ -1,11 +1,58 @@
 import 'package:flutter/material.dart';
 import 'package:word_search_app/word_search_highlight.dart';
 
-class WordSearchPuzzle extends StatelessWidget {
-    const WordSearchPuzzle({super.key, required this.rows, required this.columns});
+class WordSearchPuzzle extends StatefulWidget {
+  const WordSearchPuzzle({super.key, required this.rows, required this.columns});
 
-    final int rows;
-    final int columns;
+  final int rows;
+  final int columns;
+
+  @override
+  State<WordSearchPuzzle> createState() => _WordSearchPuzzleState();
+}
+
+class _WordSearchPuzzleState extends State<WordSearchPuzzle> {
+    final List<WordSearchHighlight> _highlights = [];
+
+    int? _pointerX;
+    int? _pointerY;
+
+    int? _activeHighlightStartX;
+    int? _activeHighlightStartY;
+    int? _activeHighlightEndX;
+    int? _activeHighlightEndY;
+
+    _setPointerLocation(int row, int column) {
+      setState(() {
+        _pointerX = column;
+        _pointerY = row;
+        
+        if(
+          _activeHighlightStartX == null
+          || _activeHighlightStartY == null
+          || _pointerX! - _activeHighlightStartX! == 0
+          || _pointerY! - _activeHighlightStartY! == 0
+          || (_pointerX! - _activeHighlightStartX!).abs() == (_pointerY! - _activeHighlightStartY!).abs()
+        ) {
+          _activeHighlightEndX = _pointerX;
+          _activeHighlightEndY = _pointerY;
+        }
+      });
+    }
+
+    _startHighlight(_) {
+      setState(() {
+        _activeHighlightStartX = _pointerX;
+        _activeHighlightStartY = _pointerY;
+      });
+    }
+
+    _cancelHighlight(_) {
+      setState(() {
+        _activeHighlightStartX = null;
+        _activeHighlightStartY = null;
+      });
+    }
 
     @override
     Widget build(BuildContext context) {
@@ -16,71 +63,53 @@ class WordSearchPuzzle extends StatelessWidget {
                 padding: EdgeInsets.all(5),
                 child: Stack(
                     children: [
-                        GridView.count(
-                            childAspectRatio: 1,
-                            crossAxisCount: columns,
-                            shrinkWrap: true,
-                            children: List.generate(rows * columns, (index) {
-                                return Container(
-                                    color: Colors.blue,
-                                    child: Center(
-                                        child: Text('X')
-                                    ),
-                                );
-                            })
+                        Listener(
+                          onPointerDown: _startHighlight,
+                          onPointerUp: _cancelHighlight,
+                          onPointerCancel: _cancelHighlight,
+                          child: GridView.count(
+                              childAspectRatio: 1,
+                              crossAxisCount: widget.columns,
+                              shrinkWrap: true,
+                              children: List.generate(widget.rows * widget.columns, (index) {
+                                  var row = (index / widget.columns).floor();
+                                  var column = index % widget.columns;
+                                          
+                                  return MouseRegion(
+                                    onEnter: (_) {
+                                      _setPointerLocation(row, column);
+                                    },
+                                    child: Container(
+                                      color: Colors.blue,
+                                      child: Center(
+                                          child: Text('X')
+                                      ),
+                                    )
+                                  );
+                              })
+                          ),
                         ),
-                        LayoutBuilder(
-                            builder: (BuildContext context, BoxConstraints constraints) {
-                                return Stack(
-                                    children: [
-                                        WordSearchHighlight(
-                                            constraints: constraints,
-                                            puzzleRows: rows,
-                                            puzzleColumns: columns,
-                                            startX: 5,
-                                            startY: 3,
-                                            endX: 9,
-                                            endY: 3
-                                        ),
-                                        WordSearchHighlight(
-                                            constraints: constraints,
-                                            puzzleRows: rows,
-                                            puzzleColumns: columns,
-                                            startX: 10,
-                                            startY: 0,
-                                            endX: 5,
-                                            endY: 0
-                                        ),
-                                        WordSearchHighlight(
-                                            constraints: constraints,
-                                            puzzleRows: rows,
-                                            puzzleColumns: columns,
-                                            startX: 1,
-                                            startY: 2,
-                                            endX: 1,
-                                            endY: 7
-                                        ),
-                                        WordSearchHighlight(
-                                            constraints: constraints,
-                                            puzzleRows: rows,
-                                            puzzleColumns: columns,
-                                            startX: 6,
-                                            startY: 6,
-                                            endX: 8,
-                                            endY: 8
-                                        ),
-                                        WordSearchHighlight(
-                                            constraints: constraints,
-                                            puzzleRows: rows,
-                                            puzzleColumns: columns,
-                                            startX: 10,
-                                            startY: 6,
-                                            endX: 12,
-                                            endY: 4
-                                        )
-                                    ]
-                                );
-                            },
+                        IgnorePointer(
+                          child: LayoutBuilder(
+                              builder: (BuildContext context, BoxConstraints constraints) {
+                                  return Stack(
+                                      children: _activeHighlightStartX != null
+                                        ? [
+                                          WordSearchHighlight(
+                                              constraints: constraints,
+                                              puzzleRows: widget.rows,
+                                              puzzleColumns: widget.columns,
+                                              startX: _activeHighlightStartX!,
+                                              startY: _activeHighlightStartY!,
+                                              endX: _activeHighlightEndX!,
+                                              endY: _activeHighlightEndY!
+                                          ),
+                                          ..._highlights
+                                        ]
+                                        : _highlights
+                                  );
+                              },
+                          ),
                         )
                     ]
                 )
